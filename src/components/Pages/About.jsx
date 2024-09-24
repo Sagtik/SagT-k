b60b3d5b0618072ef6d2139df1a796333ecafdd9
Sagtik <slsrg35@gmail.com>
2024-09-24 18:22:05 +0300

    Update About.jsx

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div>
             <Helmet>
-                <title>Lourity - Hakkımda</title>
+                <title>Sağ - Hakkımda</title>
                 <meta name="description" content="Web tabanlı uygulamalar geliştiriyor ve Discord botları ile ilgileniyorum. Portföyüm ve becerilerim için URL lourity adresini ziyaret edin." />
                 <meta property="og:image" content="/logo.png" />
                 <meta name="theme-color" content="#0ea5e9" />
@@ -21,10 +21,10 @@ const About = () => {
                 <div className='py-0 px-0 sm:py-8 sm:px-20'>
                     <div className='flex gap-2 py-8 px-20'>
                         <img className='w-12 h-12 rounded-md mt-2 sm:mt-0' src="/lourity.jpeg" alt="lourity" />
-                        <span className='font-bold text-2xl text-gray-300 sm:mt-2'>Lourity kimdir?</span>
+                        <span className='font-bold text-2xl text-gray-300 sm:mt-2'>SağTık kimdir?</span>
                     </div>
                     <div className='p-4 sm:p-1'>
-                        <h2 className='text-lg text-gray-300 font-medium'>Merhaba, ben Lourity!</h2>
+                        <h2 className='text-lg text-gray-300 font-medium'>Merhaba, ben Sağık!</h2>
                         <p className='text-gray-400 mt-1'>16 yaşında bir Full Stack Developer olarak, 4 yıldır yazılım dünyasına merak salmış, bu alanda kendimi geliştirmeye çalışan biriyim. Sakarya'da yaşıyorum ve aynı zamanda Youtube platformunda Discord botları ve yazılım ile ilgili videolar paylaşarak topluluğa katkıda bulunmaya çalışıyorum. Tüm bunlarla birlikte, lise eğitimimi sürdürürken yazılımla ilgili lisans eğitimi almayı hedefliyor ve bu alanda daha ileri düzeyde çalışmalar yapmak istiyorum.</p>
                         <h2 className='text-lg text-gray-300 font-medium mt-2'>Yazılım yolculuğum</h2>
                         <p className='text-gray-400 mt-1'>Yazılım yolculuğuma ilk adımlarımı attığımda, web tabanlı geliştirme konusunda bir tutku hissettim. Şu anda favori dillerim arasında React, HTML, CSS ve JavaScript bulunuyor. Bu dilleri kullanarak etkileyici ve kullanıcı dostu web uygulamaları geliştirmek benim için heyecan verici bir deneyim.</p>
@@ -64,12 +64,12 @@ const About = () => {
 
                     <div className='duration-300 flex rounded-md bg-mainalt-black hover:bg-mainhv-black cursor-default p-3 items-center'>
                         <img className='w-6 h-6' src="/bootstrap.svg" alt="bootstrap" />
-                        <span className='font-semibold ml-4'>Bootstrap</span>
+                        <span className='font-semibold ml-4'>Lua</span>
                     </div>
 
                     <div className='duration-300 flex rounded-md bg-mainalt-black hover:bg-mainhv-black cursor-default p-3 items-center'>
                         <img className='w-6 h-6' src="/nodejs.svg" alt="nodejs" />
-                        <span className='font-semibold ml-4'>Node JS</span>
+                        <span className='font-semibold ml-4'>C++</span>
                     </div>
 
                     <div className='duration-300 flex rounded-md bg-mainalt-black hover:bg-mainhv-black cursor-default p-3 items-center'>
